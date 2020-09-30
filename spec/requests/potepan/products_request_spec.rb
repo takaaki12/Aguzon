f31152cc4c3578dedd144ad4540f68a3c51dd7d3
takaaki <hot.roooll12@gmail.com>
2020-10-01 07:05:36 +0900

    正しいtitle表示確認のspec追加

--- spec/requests/potepan/products_request_spec.rb
+++ spec/requests/potepan/products_request_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "Potepan::Products", type: :request do
+  describe 'GET #index' do
+    it '/potepanリクエストでtitleが表示されること' do
+      get potepan_path
+      expect(response.body).to include("#{BASE_TITLE}")
+    end
+
+    it '/potepan/indexリクエストでtitleが表示されること' do
+      get potepan_path
+      expect(response.body).to include("#{BASE_TITLE}")
+    end
+  end
+
   describe 'GET #show' do
     let!(:product) { create(:product) }
       it 'リクエストが成功することを確認' do
@@ -15,7 +27,7 @@ RSpec.describe "Potepan::Products", type: :request do
 
       it 'productの値段が表示されていること' do
         get potepan_product_path product.id
-        expect(response.body).to include product.price.to_s
+        expect(response.body).to include product.display_price.to_s
       end
 
       it 'productの画像が表示されていること' do
@@ -28,6 +40,11 @@ RSpec.describe "Potepan::Products", type: :request do
       it 'productの説明文が表示されていること' do
         get potepan_product_path product.id
         expect(response.body).to include product.description.to_s
+      end
+
+      it 'productのtitleが表示されていること' do
+        get potepan_product_path product.id
+        expect(response.body).to include("#{product.name} - #{BASE_TITLE}")
       end
     end
 
