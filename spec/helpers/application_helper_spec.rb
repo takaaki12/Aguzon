96bdd05f84bca29b16448e5cad3f2e53502ecf51
takaaki <hot.roooll12@gmail.com>
2020-10-10 23:22:47 +0900

    titleがnilの場合のテスト追加

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe ApplicationHelper, type: :helper do
   describe 'Application Title helpers' do
     it "titleが正しく表示されること" do
+      expect(full_title).to eq BASE_TITLE
       expect(full_title("")).to eq BASE_TITLE
       expect(full_title("Test")).to eq "Test - #{BASE_TITLE}"
     end
