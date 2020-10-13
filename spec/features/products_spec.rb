fe0a7613f58355ce5c202b44177c557abf1e9a13
takaaki <hot.roooll12@gmail.com>
2020-10-14 07:06:48 +0900

    spec/featuresの作成

---
+++ spec/features/products_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "Products", type: :feature do
+  let!(:taxon) { create(:taxon) }
+  let!(:product) { create(:product, taxons: [taxon], price: 123) }
+
+  background do
+    visit potepan_product_path(product.id)
+  end
+
+  scenario "商品詳細にアクセスできることを確認。" do
+    within('.media-body') do
+      expect(page).to have_title product.name
+      expect(page).to have_content product.name
+      expect(page).to have_content product.display_price
+      expect(page).to have_content product.description
+    end
+  end
+
+  scenario "商品詳細からカテゴリへアクセス出来ることを確認。" do
+    click_on '一覧ページへ戻る'
+    expect(page).to have_title taxon.name
+    expect(page).to have_content product.name
+  end
+end
