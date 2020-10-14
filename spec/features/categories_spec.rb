ea7ed64404a61604f0196936f244d6f2defd3cde
takaaki <hot.roooll12@gmail.com>
2020-10-14 23:36:58 +0900

    別カテゴリの商品が表示されていないことを確認するテストの追加

--- spec/features/categories_spec.rb
+++ spec/features/categories_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Categories", type: :feature do
   let!(:product) { create(:product, taxons: [taxon]) }
   let!(:other_taxonomy) { create(:taxonomy, name: 'test_other_taxonomy') }
   let!(:other_taxon) { create(:taxon, name: 'test_ohter_taxon', parent: other_taxonomy.root) }
-  let!(:other_product) { create(:product, taxons: [other_taxon]) }
+  let!(:other_product) { create(:product, name: 'test_other_product', taxons: [other_taxon]) }
 
   background do
     visit potepan_category_path(taxon.id)
@@ -40,5 +40,9 @@ RSpec.feature "Categories", type: :feature do
   scenario "商品カテゴリ価格表示から商品詳細ページへの移動を確認する。" do
     click_on product.display_price.to_s
     expect(current_path).to eq potepan_product_path(product.id)
+  end
+
+  scenario "別カテゴリの商品が表示されていないことを確認する。" do
+    expect(page).to_not have_content other_product.name
   end
 end
