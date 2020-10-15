98e527d95da5544ca3bad77ce5f9151f9fa6f342
takaaki <hot.roooll12@gmail.com>
2020-10-16 00:29:19 +0900

    テストの可読性向上

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,11 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
-  describe 'Application Title helpers' do
-    it "titleが正しく表示されること" do
-      expect(full_title).to eq BASE_TITLE
-      expect(full_title("")).to eq BASE_TITLE
-      expect(full_title("Test")).to eq "Test - #{BASE_TITLE}"
+  describe '#full_title' do
+    context "引数が与えられなかった時" do
+      it "ベースとなるタイトル文字列だけを返す" do
+        expect(full_title).to eq 'BIGBAG Store'
+      end
+    end
+
+    context "引数に空文字が与えられた時" do
+      it "ベースとなるタイトル文字列だけを返す" do
+        expect(full_title("")).to eq BASE_TITLE
+      end
+    end
+
+    context "引数に1つ以上の文字列が与えられた時" do
+      it "与えられた文字列とベースとなるタイトル文字列が「 - 」を挟んで返される" do
+        expect(full_title("Test")).to eq "Test - #{BASE_TITLE}"
+      end
     end
   end
 end
