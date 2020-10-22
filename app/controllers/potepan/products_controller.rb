8c7619c5b273cc570c52c4ea38d6adb04e8122ad
takaaki <hot.roooll12@gmail.com>
2020-10-22 23:29:38 +0900

    productページへ関連商品をランダムで４つ表示する機能を追加

--- app/controllers/potepan/products_controller.rb
+++ app/controllers/potepan/products_controller.rb
@@ -1,6 +1,10 @@
 class Potepan::ProductsController < ApplicationController
+  DISPLAY_RELATED_PRODUCTS_NUMBER = 4
+
   def show
     @product = Spree::Product.find(params[:id])
     @images  = @product.images
+    @related_products = Spree::Product.includes(master: [:images, :default_price]).
+      related_products(@product).shuffle.take(DISPLAY_RELATED_PRODUCTS_NUMBER)
   end
 end
