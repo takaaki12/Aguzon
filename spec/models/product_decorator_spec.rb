0d5111b6a319e21be9d5e4778346cd51c421420a
takaaki <hot.roooll12@gmail.com>
2020-10-24 23:18:21 +0900

    不要な事前定義を遅延評価へ変更

--- spec/models/product_decorator_spec.rb
+++ spec/models/product_decorator_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe 'ProductDecorator', type: :model do
-  let!(:taxon) { create(:taxon) }
-  let!(:other_taxon) { create(:taxon) }
-  let!(:another_taxon) { create(:taxon) }
-  let!(:product) { create(:product, taxons: [taxon, other_taxon]) }
-  let!(:first_other_product) { create(:product, taxons: [taxon]) }
-  let!(:second_other_product) { create(:product, taxons: [other_taxon]) }
-  let!(:another_product) { create(:product, taxons: [another_taxon]) }
+  let(:taxon) { create(:taxon) }
+  let(:other_taxon) { create(:taxon) }
+  let(:another_taxon) { create(:taxon) }
+  let(:product) { create(:product, taxons: [taxon, other_taxon]) }
+  let(:first_other_product) { create(:product, taxons: [taxon]) }
+  let(:second_other_product) { create(:product, taxons: [other_taxon]) }
+  let(:another_product) { create(:product, taxons: [another_taxon]) }
 
   describe '#related_products' do
-    let!(:related_products_scope) do
+    let(:related_products_scope) do
       Spree::Product.related_products(product)
     end
 
